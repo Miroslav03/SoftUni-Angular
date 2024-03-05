3de0cad471656f55cf81fac8075c840d6041da3b
Miroslav Ivanov <znaka5@abv.bg>
2024-03-05 19:03:26 +0200

    Add get Themes and get Posts
    Add another interface

--- WorkShop-Forums/workshop-forum/src/app/api.service.ts
+++ WorkShop-Forums/workshop-forum/src/app/api.service.ts
@@ -1,18 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Theme } from './types/theme';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getThemes() {
-    const api = 'http://localhost:3000/api';
+    const { apiUrl } = environment;
+
+    return this.http.get<Theme[]>(`${apiUrl}/themes`);
   }
 
   getPosts(limit?: number) {
+    const { apiUrl } = environment;
+
+    let url = `${apiUrl}/posts`;
+
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+
+    return this.http.get<Post[]>(url);
 
   }
 }
